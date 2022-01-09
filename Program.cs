e9913c0c2924e32f5247088b8e0f191a8feec037
dyggo <dyggo@DESKTOP-NEOQB0Q>
2022-01-09 19:18:20 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Shinobi shinobi = new Shinobi("Flavio", "22", "Konoha");
+
+        Console.WriteLine(shinobi.nome);
+        Console.Read();
+    }
+}
---
+++ Shinobi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Shinobi
+{
+    public string nome = "";
+    public string idade = "";
+    public string cidade = "";
+    public Shinobi(string nome, string idade, string cidade)
+    {
+        this.nome = nome;
+        this.idade = idade;
+        this.cidade = cidade;
+    }
+}
